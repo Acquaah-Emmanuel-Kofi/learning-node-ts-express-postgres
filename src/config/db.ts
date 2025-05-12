8212cd402b4feee64232936d5766760889da3339
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-05-12 14:23:30 +0000

    feat: add connection event handlers for database pool

--- src/config/db.ts
+++ src/config/db.ts
@@ -11,4 +11,13 @@ const pool = new Pool({
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
 });
 
+pool.on("error", (err) => {
+  console.error("Error connecting to database", err);
+  process.exit(-1);
+});
+
+pool.on("connect", () => {
+  console.log("Connection pool established with database");
+});
+
 export default pool;
