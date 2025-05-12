f985013db24e2eadcf1a536e8eb016d4c84f3bbf
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-05-12 15:15:09 +0000

    feat: replace database connection test with todoRoutes integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import pool from "./config/db";
+import todoRoutes from "./routes/todoRoutes";
 
 dotenv.config();
 
@@ -13,12 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-
-// Test POSTGRES Connection
-app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT current_database();");
-  res.send(`Connected to ${result.rows[0].current_database}`);
-});
+app.use("/", todoRoutes);
 
 // Server running
 app.listen(port, () => {
